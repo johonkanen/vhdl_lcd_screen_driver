a01e2dfe1bd21a74a45d63a1df59a6bc0ff40c61
Jari Honkanen <jaarhonka@yahoo.com>
2023-04-07 23:20:02 +0300

    add array of sine values to testbench

--- testbenches/plot_sine_tb.vhd
+++ testbenches/plot_sine_tb.vhd
@@ -14,12 +14,33 @@ end;
 architecture vunit_simulation of plot_sine_tb is
 
     constant clock_period      : time    := 1 ns;
-    constant simtime_in_clocks : integer := 480;
+    constant simtime_in_clocks : integer := 480*320;
     
     signal simulator_clock     : std_logic := '0';
     signal simulation_counter  : natural   := 0;
     -----------------------------------
     -- simulation specific signals ----
+
+    constant x_max : integer := 479;
+    constant y_max : integer := 319;
+
+    signal x_pos : natural range 0 to x_max := 0;
+    signal y_pos : natural range 0 to 319 := 0;
+    type intarray is array (integer range 0 to 479) of integer;
+
+    function init_intarray return intarray
+    is
+        variable return_value : intarray;
+        constant length : real := real(intarray'length);
+    begin
+        for i in intarray'range loop
+            return_value(i) := integer(round(sin(real(i)/length*15.0*math_pi)*160.0*0.9+160.0));
+        end loop;
+        return return_value;
+        
+    end init_intarray;
+
+    signal sinearray : intarray := init_intarray;
 
 begin
 
@@ -43,8 +64,11 @@ begin
     begin
         if rising_edge(simulator_clock) then
             simulation_counter <= simulation_counter + 1;
-            write(row , integer(round(sin(real(simulation_counter)/480.0*15.0*math_pi)*160.0*0.9+160.0)));
-            writeline(f , row);
+            -- write(row , integer(round(sin(real(simulation_counter)/480.0*15.0*math_pi)*160.0*0.9+160.0)), left, 4);
+            if simulation_counter < 480 then
+                write(row , sinearray(simulation_counter));
+                writeline(f , row);
+            end if;
 
 
         end if; -- rising_edge
