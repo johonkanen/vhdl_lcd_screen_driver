58652b5dfd6d036c3c7f0986a89fbf0fff334d2a
Jari Honkanen <jaarhonka@yahoo.com>
2023-04-10 13:05:09 +0300

    move python source files into own folder

--- python_files/draw_pixel_figure_from_vhdl.py
+++ python_files/draw_pixel_figure_from_vhdl.py
@@ -7,5 +7,5 @@ xsize = 480
 ysize = 320
 
 d = np.zeros([ysize,xsize])
-data_from_vhdl = np.loadtxt("temp/pixel_image_stream_from_vhdl.txt")
+data_from_vhdl = np.loadtxt("../temp/pixel_image_stream_from_vhdl.txt")
 stream_lcd(data_from_vhdl, xsize, ysize)
