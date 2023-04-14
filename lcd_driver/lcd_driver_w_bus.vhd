ba18791186446814b479154c5ba04f5ba1e9e7a2
Jari Honkanen <jaarhonka@yahoo.com>
2023-04-14 12:02:31 +0300

    add generic for pixel transmit delay

--- lcd_driver/lcd_driver_w_bus.vhd
+++ lcd_driver/lcd_driver_w_bus.vhd
@@ -6,6 +6,7 @@ library ieee, std;
     use work.fpga_interconnect_pkg.all;
 
 entity lcd_driver_w_bus is
+    generic(delay_between_packets : integer := 4);
     port (
         clock : in std_logic;
         lcd_driver_in       : in lcd_driver_input_record;
@@ -18,7 +19,7 @@ end entity lcd_driver_w_bus;
 
 architecture write_to_file of lcd_driver_w_bus is
 
-    signal delay_counter : integer range 0 to 7 := 0;
+    signal delay_counter : integer range 0 to 2**19-1 := 0;
     signal write_address : natural := 0;
 
 begin
@@ -31,7 +32,7 @@ begin
             lcd_driver_out.pixel_has_been_written <= false;
             if lcd_driver_in.write_is_requested then
                 write_data_to_address(bus_from_lcd_driver, write_address, lcd_driver_in.pixel_to_be_written);
-                delay_counter <= 4;
+                delay_counter <= delay_between_packets;
             end if;
 
             if delay_counter > 0 then
